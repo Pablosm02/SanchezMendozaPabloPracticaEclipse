369b73c672f4dae6c0111939d4c63c48d3bf5dc3
pablosm02 <alum.psanchezm.2020@iesalixar.org>
2021-02-16 19:46:08 +0100

    commit 1

---
+++ RecuperacionECLIPSEyGIT/src/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HelloWorld {
+    // Tu programa comienza con una llamada a main().
+    // Imprime "Hola Mundo" a la ventana de la terminal.
+    public static void main(String args[])
+    {
+    	Scanner sc = new Scanner(System.in);
+    	
+        System.out.println("Hola Mundo");
+    }
+}
